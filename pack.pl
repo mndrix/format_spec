8be3b853b77d17a784c83c0272429a616e2887c6
Michael Hendricks <michael@ndrix.org>
2014-01-17 15:50:09 -0700

    Release v0.0.2

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 name(format_spec).
-title('Parse and generate format/2 specifications').
+title('Parse and check format/2 specifications').
 
-version('0.0.1').
-download('http://packs.ndrix.com/format_spec/format_spec-0.0.1.tgz').
+version('0.0.2').
+download('http://packs.ndrix.com/format_spec/format_spec-0.0.2.tgz').
 
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
